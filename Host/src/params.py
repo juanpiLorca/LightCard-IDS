6572c1ab72210ee4ff221a7b77e4a195c5880b45
jtvalladares <jtvalladares@uc.cl>
2025-05-14 15:48:19 -0400

    ip address modified

--- Host/src/params.py
+++ Host/src/params.py
@@ -1,8 +1,8 @@
 import os
 
-IP_HOST = os.getenv("IP_HOST", "192.168.0.126")
+IP_HOST = os.getenv("IP_HOST", "10.8.31.202")
 PORT = int(os.getenv("PORT", 7632))
 
-NUM_EXP = int(os.getenv("NUM_EXP", 2))
-NUM_TEST = int(os.getenv("NUM_TEST", 4))
+NUM_EXP = int(os.getenv("NUM_EXP", 1))
+NUM_TEST = int(os.getenv("NUM_TEST", 1))
 NUM_CLASS = int(os.getenv("NUM_CLASS", 1))
